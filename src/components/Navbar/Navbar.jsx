da1897061e521e98436485b0d81c65758d067b22
Levon <milevon@gmail.com>
2022-12-10 21:27:00 +0400

    navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <AppBar position="static" className="appbar">
         <Toolbar>
           <Typography>
-            <img src={logo }  alt=""/>
+            <Link to="/"><img src={logo }  alt=""/></Link>
           </Typography>
           <Box className="btn-contaner">
             <Link to="/login"><Button  sx={{color:"white"}} className="btn" size="large">Login</Button></Link>
