c262e0ed688c9c5786db2ebdc7436fb2fbcf35d0
Levon <milevon@gmail.com>
2022-12-11 21:48:41 +0400

    Irecruter

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
       <div className='home-content'>
           <TextField variant='outlined' sx={{ backgroundColor: "white", width: "40%" }} />
-          <Button variant='contained' sx={{height:"55px", backgroundColor:"#FFD733", color:"black", margin:"10px"}}>Search</Button>
+          <Button variant='contained' sx={{height:"54px", backgroundColor:"#FFD733", color:"black", margin:"10px"}}>Search</Button>
     </div>
   )
 }
